42721ea8cbc1b1a23c8dd3a94f5c3c1c4705c4cd
Blythe Fernandes <blyshobel@gmail.com>
2024-06-24 21:32:00 +0100

    File corrected

--- imgprocesslib/__init__.py
+++ imgprocesslib/__init__.py
@@ -34,18 +34,3 @@ imgdir = homedir + 'Images/'  # Path to images directory
 #     from .calibrationcpu as calibration
 #     from .imageprocesscpu as imageprocess
 #     from .makecatologuecpu as makecatologue
-"""
-import os
-import torch
-
-homedir = os.getcwd() + '/' #Path to home dir
-ddir = homedir + 'Jades_files/' #Path to data dir
-imgdir = homedir + 'Images/'
-
-# if torch.cuda.is_available():
-#     # run calibration.py if GPU is available else run calibration_cpu.py
-#     from . import calibration, imageprocess, makecatologue
-# else:
-#     from .calibrationcpu as calibration
-#     from .imageprocesscpu as imageprocess
-#     from .makecatologuecpu as makecatologue
